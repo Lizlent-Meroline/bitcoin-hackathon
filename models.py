33b38072aedc1f73817be667b04d42726ad58342
ibrsamwel <ibraah568@gmail.com>
2026-06-11 22:29:57 +0300

    feat: initialize models.py with imports and Base

---
+++ models.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
